fb35b5fdb49497c0e46036af5352392408a05fed
Shagun <shagunpadha108@gmail.com>
2025-12-08 15:51:08 +0530

    Update logger.h

--- include/logger.h
+++ include/logger.h
@@ -1,18 +1,1 @@
-#ifndef LOGGER_H
-#define LOGGER_H
 
-#include <string>
-#include <fstream>
-
-class Logger
-{
-private:
-    std::ofstream file;
-
-public:
-    Logger(const std::string &filename);
-    void log(const std::string &message);
-    ~Logger();
-};
-
-#endif
