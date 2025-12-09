8fa109f4794f7c6ddc9ea48397b5464cc3b2f0b3
shagun-coder <shagunpadha108@gmail.com>
2025-12-09 13:04:11 +0530

    Add logs folder for Logger

--- include/logger.h
+++ include/logger.h
@@ -1,17 +1,16 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-class Logger{
-    private:
+class Logger
+{
+private:
     // variabl store file name in which log is saved
     string eventFile;
-    public:
-    // constructor 
-    Logger(const string& filename);
+
+public:
+    // constructor
+    Logger(const string &filename);
     // To records events -> level + message
-     void log(const string& level, const string& message);
-    
-
-
+    void log(const string &level, const string &message);
 };
--- src/logger.cpp
+++ src/logger.cpp
@@ -1,33 +1,34 @@
 #include "Logger.h"
-#include<fstream>
-#include<ctime>
-#include<cstring>
+#include <fstream>
+#include <ctime>
+#include <cstring>
 using namespace std;
 
-Logger::Logger(const string &filename){
-        eventFile=filename;
+Logger::Logger(const string &filename)
+{
+    eventFile = filename;
 }
-void Logger::log(const string &level, const string &message){
+void Logger::log(const string &level, const string &message)
+{
 
     ofstream myfile;
 
     // file open in append mode
-    myfile.open(eventFile,ios::app);
+    myfile.open(eventFile, ios::app);
 
     // open check
-    if(!myfile.is_open()){
-        cout<<"Error : cannot open log file!"<<endl;
+    if (!myfile.is_open())
+    {
+        cout << "Error : cannot open log file!" << endl;
         return;
-
     }
-    
+
     // Time extract
-    time_t currentTime= time(0);
-    char* dt = ctime(&currentTime);
+    time_t currentTime = time(0);
+    char *dt = ctime(&currentTime);
 
     // file write
-    myfile << "["<<dt<<"]"<<" "<<"["<<level <<"]"<<message<<endl;
+    myfile << "[" << dt << "]" << " " << "[" << level << "]" << message << endl;
 
     myfile.close();
 }
- 
