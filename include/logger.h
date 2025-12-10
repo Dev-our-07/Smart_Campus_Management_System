2acee48c33797f58584320de8516c08f62d81759
Shagun <shagunpadha108@gmail.com>
2025-12-10 23:40:23 +0530

    Update logger.h

--- include/logger.h
+++ include/logger.h
@@ -1,18 +1,18 @@
 #ifndef LOGGER_H
 #define LOGGER_H
+
 #include <string>
 using namespace std;
 
 class Logger
 {
 private:
-    // variabl store file name in which log is saved
-    string eventFile;
+    string logfile;
 
 public:
-    // constructor
-    Logger(const string &filename);
-    // To records events -> level + message
-    void log(const string &level, const string &message);
+    Logger();
+    Logger(string filename);
+    void log(string level, string module, string message);
 };
-#endif // LOGGER_H
+
+#endif
